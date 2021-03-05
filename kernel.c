725000cb447f11b4f89f2f9a3576112125727cbd
PhilBaird <plipmana@gmail.com>
2021-03-05 15:59:48 -0500

    spoooky

--- kernel.c
+++ kernel.c
@@ -15,7 +15,7 @@
 #define output "output.txt"
 
 struct Process {
-    int PID, Arrival_Time, Total_CPU_Time, IO_Frequency, IO_Duration, runningC,waitingC;
+    int PID, Arrival_Time, Total_CPU_Time, IO_Frequency, IO_Duration, runningC, waitingC;
 };
 
 struct Process ready[arrlen], running[arrlen], waiting[arrlen];
@@ -40,13 +40,59 @@ void clearOutput();
 
 void printFile(int t, int PID, char *s1, char *s2);
 
+void fcfs();
+
+void ep();
+
+void rr();
+
+
 
 int readyLen = 0, runningLen = 0, waitingLen = 0, tick = 0;
 //int PID, Arrival_Time, Total_CPU_Time, IO_Frequency, IO_Duration;
 //int runningC, waitingC;
-int main(int argc, char *argv[]) {
+int main() {
+
+    char strScheduler[20];
+    int scheduler = 0;
+    while(scheduler == 0){
+        printf("Scheduler to use: fcfs, ep, rr ?\n");
+        fgets(strScheduler,20,stdin);
+        if (strstr(strScheduler, "fcfs") != NULL) {
+            scheduler = 1;
+        }
+        else if (strstr(strScheduler, "ep") != NULL) {
+            scheduler = 2;
+        }
+        else if (strstr(strScheduler, "rr") != NULL) {
+            scheduler = 3;
+        }
+        else {
+            printf("Wrong choice ?\n");
+        }
+    }
+
     clearOutput();
-    readFile();
+    switch(scheduler){
+        case 1:
+            readFile();
+            fcfs();
+            break;
+        case 2:
+            readFile();
+            ep();
+            break;
+        case 3:
+            readFile();
+            rr();
+            break;
+    }
+
+
+    return 0;
+}
+
+void fcfs(){
     // to make sure max only one transition per tick
     int breakout = 0;
     // loops for each tick
@@ -63,7 +109,7 @@ int main(int argc, char *argv[]) {
                     terminate(ptr->PID);
                     breakout = 1;
                 }
-                // if the process didnt just start and it is evenly divisible by the io frequency and no other action has happnened
+                    // if the process didnt just start and it is evenly divisible by the io frequency and no other action has happnened
                 else if (ptr->runningC > 0 && ptr->runningC % ptr->IO_Frequency == 0  && breakout == 0) {
                     printf("%i -> wait: %i\n",tick, ptr->PID);
                     printFile(tick, ptr->PID, "RUNNING", "WAITING");
@@ -113,8 +159,155 @@ int main(int argc, char *argv[]) {
         }
         tick++;
     }
-    return 0;
 }
+
+void ep(){
+    // to make sure max only one transition per tick
+    int breakout = 0;
+    // loops for each tick
+    while(tick < ticks){
+        breakout = 0;
+        // if there is process in the running array, should never be more than one
+        if(runningLen > 0){
+            struct Process* ptr = running;
+            for (int i=0; i<runningLen; i++, ptr++ ) {
+                // if the running count for the process greater than the total cpu time and no other action has happened
+                if (ptr->runningC >= ptr->Total_CPU_Time  && breakout == 0){
+                    printf("%i -> terminate: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "RUNNING", "TERMINATED");
+                    terminate(ptr->PID);
+                    breakout = 1;
+                }
+                    // if the process didnt just start and it is evenly divisible by the io frequency and no other action has happnened
+                else if (ptr->runningC > 0 && ptr->runningC % ptr->IO_Frequency == 0  && breakout == 0) {
+                    printf("%i -> wait: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "RUNNING", "WAITING");
+                    // the processes is moved to waiting
+                    wait(ptr->PID);
+                    breakout = 1;
+                }
+                ptr->runningC ++;
+
+
+
+            }
+        }
+        // if there are processes in the ready array
+        if(readyLen > 0){
+            // go through all the processes
+            struct Process* ptr = ready;
+            prioties = arrlen;
+
+            for (int i=0; i<readyLen; i++, ptr++ ) {
+                if ptr->prioties < prioties {
+                    prioties = ptr->prioties
+
+                }
+            }
+            for (int i=0; i<readyLen; i++, ptr++ ) {
+                // if the running array is empty and the tick is after the arrival time and there has not been a action preformed yet
+                if (runningLen == 0 && tick >= ptr->Arrival_Time  && breakout == 0 && ptr->prioties = prioties) {
+                    printf("%i -> dispatch: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "READY", "RUNNING");
+                    // move the process to running
+                    dispatch(ptr->PID);
+                    breakout = 1;
+
+                }
+            }
+        }
+
+        // if there is processes in the waiting array
+        if(waitingLen > 0){
+            struct Process* ptr = waiting;
+            for (int i=0; i<waitingLen; i++, ptr++ ) {
+                // if the process didnt just start and is the time waiting for io is divisible by the io duration
+                if (ptr->waitingC > 0 && ptr->waitingC % ptr->IO_Duration == 0  && breakout == 0) {
+                    printf("%i -> done: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "WAITING", "READY");
+                    // move to ready
+                    done(ptr->PID);
+                    breakout = 1;
+                }
+                // each process in waiting increases
+                ptr->waitingC ++;
+
+            }
+        }
+        tick++;
+    }
+}
+
+void rr(){
+    // to make sure max only one transition per tick
+    int breakout = 0;
+    // loops for each tick
+    while(tick < ticks){
+        breakout = 0;
+        // if there is process in the running array, should never be more than one
+        if(runningLen > 0){
+            struct Process* ptr = running;
+            for (int i=0; i<runningLen; i++, ptr++ ) {
+                // if the running count for the process greater than the total cpu time and no other action has happened
+                if (ptr->runningC >= ptr->Total_CPU_Time  && breakout == 0){
+                    printf("%i -> terminate: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "RUNNING", "TERMINATED");
+                    terminate(ptr->PID);
+                    breakout = 1;
+                }
+                    // if the process didnt just start and it is evenly divisible by the io frequency and no other action has happnened
+                else if (ptr->runningC > 0 && ptr->runningC % ptr->IO_Frequency == 0  && breakout == 0) {
+                    printf("%i -> wait: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "RUNNING", "WAITING");
+                    // the processes is moved to waiting
+                    wait(ptr->PID);
+                    breakout = 1;
+                }
+                ptr->runningC ++;
+
+
+
+            }
+        }
+        // if there are processes in the ready array
+        if(readyLen > 0){
+            // go through all the processes
+            struct Process* ptr = ready;
+            for (int i=0; i<readyLen; i++, ptr++ ) {
+
+                // if the running array is empty and the tick is after the arrival time and there has not been a action preformed yet
+                if (runningLen == 0 && tick >= ptr->Arrival_Time  && breakout == 0) {
+                    printf("%i -> dispatch: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "READY", "RUNNING");
+                    // move the process to running
+                    dispatch(ptr->PID);
+                    breakout = 1;
+
+                }
+            }
+        }
+
+        // if there is processes in the waiting array
+        if(waitingLen > 0){
+            struct Process* ptr = waiting;
+            for (int i=0; i<waitingLen; i++, ptr++ ) {
+                // if the process didnt just start and is the time waiting for io is divisible by the io duration
+                if (ptr->waitingC > 0 && ptr->waitingC % ptr->IO_Duration == 0  && breakout == 0) {
+                    printf("%i -> done: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "WAITING", "READY");
+                    // move to ready
+                    done(ptr->PID);
+                    breakout = 1;
+                }
+                // each process in waiting increases
+                ptr->waitingC ++;
+
+            }
+        }
+        tick++;
+    }
+}
+
 
 
 

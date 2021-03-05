49d3eb982a0c57f07b01aab8de5590bd6d118bdf
PhilBaird <plipmana@gmail.com>
2021-03-05 15:18:52 -0500

    working part 1

--- kernel.c
+++ kernel.c
@@ -11,6 +11,8 @@
 
 #define arrlen 20
 #define ticks 120
+#define input "input.txt"
+#define output "output.txt"
 
 struct Process {
     int PID, Arrival_Time, Total_CPU_Time, IO_Frequency, IO_Duration, runningC,waitingC;
@@ -34,44 +36,38 @@ void interrupt(int PID);
 
 void readFile();
 
-void readFile2();
+void clearOutput();
 
-const char* getField(char* line, int num);
+void printFile(int t, int PID, char *s1, char *s2);
 
-int getline(char **pString, size_t *pInt, FILE *pIobuf);
 
 int readyLen = 0, runningLen = 0, waitingLen = 0, tick = 0;
 //int PID, Arrival_Time, Total_CPU_Time, IO_Frequency, IO_Duration;
 //int runningC, waitingC;
-int main(void) {
-
+int main(int argc, char *argv[]) {
+    clearOutput();
     readFile();
-
+    // to make sure max only one transition per tick
     int breakout = 0;
+    // loops for each tick
     while(tick < ticks){
         breakout = 0;
-//        printf("\nt: %i", tick );
-        if(readyLen > 0){
-            struct Process* ptr = ready;
-            for (int i=0; i<readyLen; i++, ptr++ ) {
-                if (runningLen == 0 && tick >= ptr->Arrival_Time  && breakout == 0) {
-                    printf("\n%i -> dispatch: %i",tick, ptr->PID);
-                    dispatch(ptr->PID);
-                    breakout = 1;
-
-                }
-            }
-        }
+        // if there is process in the running array, should never be more than one
         if(runningLen > 0){
             struct Process* ptr = running;
             for (int i=0; i<runningLen; i++, ptr++ ) {
+                // if the running count for the process greater than the total cpu time and no other action has happened
                 if (ptr->runningC >= ptr->Total_CPU_Time  && breakout == 0){
-                    printf("\n%i -> terminate: %i",tick, ptr->PID);
+                    printf("%i -> terminate: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "RUNNING", "TERMINATED");
                     terminate(ptr->PID);
                     breakout = 1;
                 }
+                // if the process didnt just start and it is evenly divisible by the io frequency and no other action has happnened
                 else if (ptr->runningC > 0 && ptr->runningC % ptr->IO_Frequency == 0  && breakout == 0) {
-                    printf("\n%i -> wait: %i",tick, ptr->PID);
+                    printf("%i -> wait: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "RUNNING", "WAITING");
+                    // the processes is moved to waiting
                     wait(ptr->PID);
                     breakout = 1;
                 }
@@ -81,14 +77,36 @@ int main(void) {
 
             }
         }
+        // if there are processes in the ready array
+        if(readyLen > 0){
+            // go through all the processes
+            struct Process* ptr = ready;
+            for (int i=0; i<readyLen; i++, ptr++ ) {
+                // if the running array is empty and the tick is after the arrival time and there has not been a action preformed yet
+                if (runningLen == 0 && tick >= ptr->Arrival_Time  && breakout == 0) {
+                    printf("%i -> dispatch: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "READY", "RUNNING");
+                    // move the process to running
+                    dispatch(ptr->PID);
+                    breakout = 1;
+
+                }
+            }
+        }
+
+        // if there is processes in the waiting array
         if(waitingLen > 0){
             struct Process* ptr = waiting;
             for (int i=0; i<waitingLen; i++, ptr++ ) {
+                // if the process didnt just start and is the time waiting for io is divisible by the io duration
                 if (ptr->waitingC > 0 && ptr->waitingC % ptr->IO_Duration == 0  && breakout == 0) {
-                    printf("\n%i -> done: %i",tick, ptr->PID);
+                    printf("%i -> done: %i\n",tick, ptr->PID);
+                    printFile(tick, ptr->PID, "WAITING", "READY");
+                    // move to ready
                     done(ptr->PID);
                     breakout = 1;
                 }
+                // each process in waiting increases
                 ptr->waitingC ++;
 
             }
@@ -102,19 +120,26 @@ int main(void) {
 
 void admitted(struct Process process) {
     // gone -> ready
-    printf("\nadmitted: %i", process.PID);
+    printf("admitted: %i\n", process.PID);
+    // adds process to the end of the ready array
     ready[readyLen] = process;
     readyLen ++;
 }
 
 void dispatch(int PID) {
     // ready -> running
+    // goes through each process in the ready array
     struct Process* ptr = ready;
     for (int i=0; i<readyLen; i++, ptr++ ) {
+        // finds the matching pid
         if(ptr->PID == PID){
+            // adds process to the running array
             running[runningLen] = *ptr;
+            // increments running len
             runningLen ++;
+            // removes the process from the ready array
             remove_element(ready, i, readyLen);
+            // decrements the ready len
             readyLen --;
         }
     }
@@ -171,14 +196,19 @@ void terminate(int PID){
         }
     }
 }
-
+/**
+ * removes the element at selected index from the array
+ */
 void remove_element(struct Process array[arrlen], int index, int array_length) {
     for(int i = index; i < array_length - 1; i++) array[i] = array[i + 1];
 }
 
+/**
+ * reads in file from the input filename
+ */
 void readFile()
 {
-    FILE* file = fopen ("input.txt", "r");
+    FILE* file = fopen (input, "r");
     int i,i1,i2,i3,i4;
     while (!feof (file))
     {
@@ -199,6 +229,32 @@ void readFile()
     fclose (file);
 
 }
+
+/**
+ * prints one line to the output file
+ * @param t
+ * @param PID
+ * @param s1
+ * @param s2
+ */
+void printFile(int t, int PID, char *s1, char *s2)
+{
+    FILE* file = fopen (output, "a");
+    fprintf (file, "%d %d %s %s\n", t, PID, s1, s2);
+    fclose (file);
+}
+
+/**
+ * clears the output file
+ */
+void clearOutput()
+{
+    FILE* file = fopen (output, "w");
+    fclose (file);
+}
+
+
+
 
 
 
